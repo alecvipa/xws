e24e7d59c38779a4bcb932e074f7f64da1c4c958
alecvipa <hebrit_626@hotmail.com>
2024-01-03 12:30:16 -0600

    added new

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,7 @@
 if (window.location.href == "https://www.xyncs.com/xyncsweb") {
     $(".navBarD").hide();
 }
+
+if (window.location.href == "https://www.xyncs.com/xyncsweb") {
+    $("#fotterSmth").hide();
+}
